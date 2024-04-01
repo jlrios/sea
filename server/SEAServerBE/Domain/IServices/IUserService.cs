48327f046b7d9f2306da579231e5e6cd24bbb526
jlrios <rdjl5791@gmail.com>
2024-04-01 04:21:52 -0600

    The backend project has been added.

---
+++ server/SEAServerBE/Domain/IServices/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using SEAServerBE.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEAServerBE.Domain.IServices
+{
+    public interface IUserService
+    {
+        Task SaveUser(User user);
+        Task<bool> ValidateExistence(User user);
+        Task<User> ValidatePassword(int idUser, String oldPassword);
+    }
+}
---
+++ server/SEAServerBE/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEAServerBE.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEAServerBE.Persistence.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
